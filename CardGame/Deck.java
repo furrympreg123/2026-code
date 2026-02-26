302fd1766c0a2106bb344e01a89a1e75481da136
kanyawhs <166661560+kanyawhs@users.noreply.github.com>
2026-02-27 10:27:59 +1300

    Add files via upload

---
+++ CardGame/Deck.java
@@ -0,0 +1,51 @@
+
+/**
+ * Write a description of class Deck here.
+ *
+ * @author Kanya Farley
+ * @version 27/2/26
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+public class Deck
+{
+    ArrayList<Card> myDeck = new ArrayList<Card>();
+    /**
+     * Constructor for objects of class Deck
+     */
+    public Deck()
+    {
+        /**
+         * add two arrays (suits and faces),  
+
+        this time rather than selecting one of each, use nested loops to
+        select each variation of a card's face and suit, 
+
+        Use each combination of these to create a new 'Card' object and, 
+
+        Add the 'Card' object to the ArrayList you created
+         */
+
+        String[] suit = {"Hearts", "Clubs", "Diamonds", "Spades"};
+        String[] face = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+        // selects each variation of suit + face
+        for (int i = 0; i < suit.length; i++) {
+            for (int j = 0; j < face.length; j++) {
+                Card currentCard = new Card (suit[i], face[j]);
+                myDeck.add(currentCard);
+                System.out.println(currentCard.getSuit() + currentCard.getFace());
+            }
+        }
+
+    }
+    
+    public void shuffle() {
+        Collections.shuffle(this.myDeck);
+    }
+    
+    public Card popCard() {
+        return (myDeck.remove(0));
+    }
+}
--- CardGame/main.java
+++ CardGame/main.java
@@ -3,7 +3,7 @@
  * This creates new objects in cahoots with the Card class
  *
  * @author Kanya Farley
- * @version 23/02/2026
+ * @version 27/02/2026
  */
 import java.util.Random;
 import java.util.ArrayList;
@@ -19,48 +19,82 @@ public class main
         Random random = new Random();
         String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
         String[] faces = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-        
+
         // prints random face
         String randomFace = faces[random.nextInt(faces.length)];
         //System.out.println(randomFace);
-        
+
         // prints random suit
         String randomSuit = suits[random.nextInt(suits.length)];
         //System.out.println(randomSuit);
-        
-        
-        
+
         // creates new random card
         Card card1 = new Card(randomFace, randomSuit);
-        System.out.println(card1.getSuit());
-        System.out.println(card1.getFace() + " (Value: " + card1.getValue() + ")");
-        //for (int i = 0; i < Card.size; i++) {
-            
-        //}
-        
+        System.out.println(card1.getFace() + " " + card1.getSuit());
+        System.out.println(" (Value: " + card1.getValue() + ")");
+
         Card card2 = new Card();
         System.out.println(card2);
-        
+
         Card card3 = new Card();
         System.out.println(card3);
-        
+
         Card card4 = new Card();
         System.out.println(card4);
-        
-        userPrintsCards();
-        
+
     }
     
+    // this lets user decide number of random cards printed
     public static void userPrintsCards () {
         Scanner kb = new Scanner(System.in);
         System.out.println("How many cards would you like me to print?");
         int cards = kb.nextInt();
-        ArrayList<String>cardCreator = new ArrayList<String>();
+
+        // creates cards according to users input
+        ArrayList<Card>cardCreator = new ArrayList<Card>();
         for (int i = 0; i < cards; i++) {
-            cardCreator.add("card" + i);
+            cardCreator.add(new Card());
         }
-        System.out.println(cardCreator);
+        //System.out.println(cardCreator); // just checks how many cards were made
+
+        // prints suits, faces, and values of cards
+        for (int i = 0; i < cardCreator.size(); i++) {
+            Card currentCard = cardCreator.get(i);
+            System.out.println(currentCard.getFace() + " " + currentCard.getSuit());
+            System.out.println("Value : " + currentCard.valueOfFace()); // it's always 0??
+        }
+
+        // adds card values up
+        int totalValue = 0;
+        for (int i = 0; i < cardCreator.size(); i++) {
+            Card currentCard = cardCreator.get(i);
+            totalValue += currentCard.valueOfFace();
+        }
+        System.out.println(totalValue);
     }
     
-    
+    // this allows user to decide if they want to add another random card to an array or finish it
+    public static void userUnsure () {
+
+        Scanner kb = new Scanner(System.in);
+        boolean active = true;
+        ArrayList<Card>cardCreator = new ArrayList<Card>();
+        while (active) {
+            System.out.println("Add another random card? Type 'yes' or 'no'");
+            String input = kb.nextLine();
+            if (input.equals("yes") || input.equals("YES") || input.equals("Yes")) {
+                cardCreator.add(new Card());
+                
+                // fetches + prints new card
+                for (int i = 0; i < cardCreator.size(); i++) {
+                    Card currentCard = cardCreator.get(i);
+                }
+            } else if (input.equals("no") || input.equals("NO") || input.equals("No")) {
+                System.out.println("Okay!");
+                active = false; // stops loop
+            } else {
+                System.out.println("Sorry, I don't understand...");
+            }
+        }
+    }
 }
